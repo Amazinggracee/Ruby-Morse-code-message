af4b0b970ea50fe108354703bb6fd6b727cff1ca
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-06-14 11:14:59 +0100

    Add the morse content

--- morse_code.rb
+++ morse_code.rb
@@ -1,59 +1,60 @@
-MORSE_CODE = {
-  '-----' => '0',
-  '.----' => '1',
-  '..---' => '2',
-  '...--' => '3',
-  '....-' => '4',
-  '.....' => '5',
-  '-....' => '6',
-  '--...' => '7',
-  '---..' => '8',
-  '----.' => '9',
-  '.-' => 'a',
-  '-...' => 'b',
-  '-.-.' => 'c',
-  '-..' => 'd',
-  '.' => 'e',
-  '..-.' => 'f',
-  '--.' => 'g',
-  '....' => 'h',
-  '..' => 'i',
-  '.---' => 'j',
-  '-.-' => 'k',
-  '.-..' => 'l',
-  '--' => 'm',
-  '-.' => 'n',
-  '---' => 'o',
-  '.--.' => 'p',
-  '--.-' => 'q',
-  '.-.' => 'r',
-  '...' => 's',
-  '-' => 't',
-  '..-' => 'u',
-  '...-' => 'v',
-  '.--' => 'w',
-  '-..-' => 'x',
-  '-.--' => 'y',
-  '--..' => 'z',
-  '.-.-.-' => '.',
-  '--..--' => ',',
-  '..--..' => '?',
-  '-.-.--' => '!',
-  '-....-' => '-',
-  '-..-.' => '/',
-  '.--.-.' => '@',
-  '-.--.' => '(',
-  '-.--.-' => ')'
-}.freeze
+class MorseDecoder
+  MORSE_CODE = {
+    '.-'   => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..'  => 'D',
+    '.'    => 'E',
+    '..-.' => 'F',
+    '--.'  => 'G',
+    '....' => 'H',
+    '..'   => 'I',
+    '.---' => 'J',
+    '-.-'  => 'K',
+    '.-..' => 'L',
+    '--'   => 'M',
+    '-.'   => 'N',
+    '---'  => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.'  => 'R',
+    '...'  => 'S',
+    '-'    => 'T',
+    '..-'  => 'U',
+    '...-' => 'V',
+    '.--'  => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z',
+    '.----' => '1',
+    '..---' => '2',
+    '...--' => '3',
+    '....-' => '4',
+    '.....' => '5',
+    '-....' => '6',
+    '--...' => '7',
+    '---..' => '8',
+    '----.' => '9',
+    '-----' => '0'
+  }
 
-def decode_char(char)
-  MORSE_CODE[char]
+  def self.decode_char(morse)
+    MORSE_CODE[morse]
+  end
+
+  def self.decode_word(morse_word)
+    morse_chars = morse_word.split(' ')
+    decoded_word = morse_chars.map { |morse| decode_char(morse) }
+    decoded_word.join('')
+  end
+
+  def self.decode(message)
+    morse_words = message.split('   ')
+    decoded_message = morse_words.map { |morse_word| decode_word(morse_word) }
+    decoded_message.join(' ')
+  end
 end
 
-def decode_word(word)
-  word.split.map { |char| decode_char(char) }.join
-end
-
-def decode_message(message)
-  message.split('   ').map { |word| decode_word(word) }.join(' ')
-end
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = MorseDecoder.decode(message)
+puts decoded_message
